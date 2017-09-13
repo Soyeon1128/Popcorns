13c68bb72f62a26c71e4a4f8cef34a781251b7d4
soyeon1128 <ksy4334334@gmail.com>
2017-09-13 17:55:22 +0900

    초기 세팅 및 API 테스트

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Popcorns
+
+## 서비스 소개
+- 영화 인기 순위 및 정보 검색 서비스
+
+## MVP (Minimal Viable Product)
+
+  #### 1. User
+  - Sign In
+  - Sign Up
+  - Bookmark
+
+  #### 2. Rating (순위)
+  - Popular Rating
+
+  #### 3. Search (검색) & Information (정보)
+  - Title
+  - Poster Image
+  - Release Date
+  - Genres
+  - Overview
+  - Tagline
+  - Actor/Actress
+  - Director
+  - Production Companies
+  - Production Countries
+
+## 사용 기술
+- Vue.js
+- Vuex
+- Vue-router
+- axios
+- webpack
+- Firebase
+
+## 영화 정보 API
+- [THE MOVIE DB](www.themoviedb.org)
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+
+    <!--헤더 영역-->
+    <header class="header">
+      <h2>HEADER</h2>
+    </header>
+
+    <!--메인 영역-->
+    <div class="main">
+      <div class="rank-area">
+        <h2 @click="getData">순위</h2>
+      </div>
+      <div class="search-area">
+        <h2>검색</h2>        
+      </div>
+    </div>
+
+    <!--푸터 영역-->
+    <footer class="footer">
+      <h2>FOOTER</h2>
+    </footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  methods : {
+    getData() {
+      this.$http.get("http://api.themoviedb.org/3/movie/198277/casts?api_key=d699f344a468b575a26d5ea50f0265f4&language=ko", {
+
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  },
+}
+</script>
+
+<style>
+/*헤더 영역*/
+.header {
+  background: yellow;
+}
+
+/*메인 영역*/
+.main {
+  background: pink;
+}
+.rank-area {
+  background: orange;  
+}
+.search-area {
+  background: green;  
+}
+
+/*푸터 영역*/
+.footer {
+  background: red;  
+}
+</style>
