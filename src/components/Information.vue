647c910fa30830a17d45b1de361feab9bc8a8298
soyeon1128 <ksy4334334@gmail.com>
2017-10-03 18:33:36 +0900

    Information 템플릿 수정(장르, 제작사 배열)

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -6,9 +6,14 @@
       <div class="info-original-title"> {{ movieData.original_title }} </div>
       <div class="info-vote"> {{ movieData.vote_average }} </div>
       <div class="info-date"> {{ movieData.release_date }} </div>
-      <div class="info-genres"> {{ movieData.genres }} </div>
-      <div class="info-countries"> {{ movieData.production_countries }} </div>
-      <div class="info-companies"> {{ movieData.production_companies }} </div>
+      <div class="info-genre" v-for="(item,index) in genre_array" :key="genre_array[index]">
+        <div class="genre-item"> {{ item }} </div>
+      </div>
+      <div class="info-company" v-for="(item,index) in company_array" :key="company_array[index]">
+        <div class="company-item"> {{ item }} </div>
+      </div>
+      <!--<div class="info-countries"> {{ movieData.production_countries }} </div>-->
+      <!--<div class="info-companies"> {{ movieData.production_companies }} </div>-->
       <div class="info-runtime"> {{ movieData.runtime }} </div>
       <div class="info-popularity"> {{ movieData.popularity }} </div>
       <div class="info-tagline"> {{ movieData.tagline }} </div>
@@ -28,6 +33,8 @@ export default {
   data () {
     return {
       movieData: "",
+      genre_array: [],
+      company_array: []
     }
   },
   methods: {
@@ -38,6 +45,19 @@ export default {
       .then(response => {
         console.log(response.data);
         this.movieData = response.data;
+
+        // 장르 배열
+        let genre_length = this.movieData.genres.length;
+        for(let i=0, l=genre_length; i<l; i++) {
+          this.genre_array.push(this.movieData.genres[i].name);
+        }
+
+        // 제작사 배열
+        let company_length = this.movieData.production_companies.length;
+        for(let i=0, l=company_length; i<l; i++) {
+          this.company_array.push(this.movieData.production_companies[i].name);
+        }
+
       })
       .catch(error => {
         console.log(error);
